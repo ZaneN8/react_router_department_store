bc2dae1624c270d1a48aaa6c8cef077c0a733f19
James Yeates <jimibue@gmail.com>
2020-09-24 16:28:01 -0600

    update and create items working

--- client/src/departments/Item.js
+++ client/src/departments/Item.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const Item = ({ name, price, id }) => {
+const Item = ({ name, price, id, departmentId }) => {
   return (
     <div>
       {name}
       <p>${price}</p>
+      <Button
+        as={Link}
+        to={{
+          pathname: `/departments/${departmentId}/items/${id}/edit`,
+          state: { name, price },
+        }}
+      >
+        edit
+      </Button>
     </div>
   );
 };
